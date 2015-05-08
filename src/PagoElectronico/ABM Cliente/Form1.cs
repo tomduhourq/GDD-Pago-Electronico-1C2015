fd10c0cb6012ccf162b75486310d993b9ada2b1a
tomduhourq <t.duhourq@gmail.com>
2015-05-08 00:34:01 -0300

    Corrección de las forms mal generadas

--- src/PagoElectronico/ABM Cliente/Form1.cs
+++ src/PagoElectronico/ABM Cliente/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace PagoElectronico.ABM_Rol
+namespace PagoElectronico.ABM_Cliente
 {
     public partial class Form1 : Form
     {
