4521b0bbb8f88f138f9f2d3eaca01a69acb818ed
fedebonisconti <fedebonisconti26@gmail.com>
2015-05-26 16:27:53 -0300

    Pre-Modelos de Funcionalidad y Rol

---
+++ src/PagoElectronico/Models/Funcionalidad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using PagoElectronico.Models.DB;
+
+namespace PagoElectronico.Models
+{
+    public class Funcionalidad
+    {
+        public string Name { get; set; }
+
+        public Funcionalidad() { }
+    }
+}
