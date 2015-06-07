98a14b1057e1339bb293bb6a16236ee00c8bc7ec
tomduhourq <t.duhourq@gmail.com>
2015-06-06 22:33:37 -0300

    Dejo el login como principal

--- src/PagoElectronico/Program.cs
+++ src/PagoElectronico/Program.cs
@@ -17,7 +17,7 @@ namespace PagoElectronico
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmABMRol());
+            Application.Run(new frmLogin());
         }
     }
 }
